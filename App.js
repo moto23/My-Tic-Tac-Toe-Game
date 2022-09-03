945c6b7e83d20b219ec4ec114ad08e01f37d412f
Prasad Nathe <99172728+moto23@users.noreply.github.com>
2022-09-03 16:00:14 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,41 @@
+
+import Board from './Board';
+import Info from "./Info";
+
+
+import "./css/app.css";
+
+
+import { useState } from 'react';
+
+function App() {
+
+	
+	const [reset, setReset] = useState(false);
+
+	const [winner, setWinner] = useState('');
+
+	const resetBoard = () => {
+		setReset(true);
+	}
+
+	return (
+		<div className="App">
+			{/* Shrinks the popup when there is no winner */}
+			<div className={`winner ${winner !== '' ? '' : 'shrink'}`}>
+				{/* Display the current winner */}
+				<div className='winner-text'>{winner}</div>
+				{/* Button used to reset the board */}
+				<button onClick={() => resetBoard()}>
+					Reset Board
+				</button>
+			</div>
+			{}
+			<Board reset={reset} setReset={setReset} winner={winner}
+				setWinner={setWinner} />
+			<Info />
+		</div>
+	);
+}
+
+export default App;
